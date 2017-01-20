e0b65e49874a351011ff5bb45cce4b25cd897605
endre bakken stovner <endrebakkenstovner@ans-180249.stolav.ntnu.no>
2017-01-20 16:00:53 +0100

    Gotten further on C

--- c/compute_lengths_idxes.c
+++ c/compute_lengths_idxes.c
@@ -1,22 +1,21 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "minunit.h"
 
 int tests_run = 0;
 
-/*
-function that
 
-1 creates new arrays for new_lengths, a_indexes, b_indexes
-2 extends run lengths
-3 computes the new run lengths and indexes
-*/
+struct Rle {
+  int *ls;
+  int *a;
+  int *b;
+  int size;
+};
 
 
-static int * run_length(int *ls1, int ls1_length, int *ls2, int ls2_length) { // # }, int *new_lengths) {
-
-  /* mu_assert("lengths not equal!", ls1_length == ls2_length); */
+static struct Rle run_length(int *ls1, int ls1_length, int *ls2, int ls2_length) {
 
   int ax = 0;
   int bx = 0;
@@ -24,14 +23,21 @@ static int * run_length(int *ls1, int ls1_length, int *ls2, int ls2_length) { //
   int a_rem = ls1[0];
   int b_rem = ls2[0];
 
-  int maxlen = ls1_length + ls2_length;
-  int *new_lengths = (int *)malloc(sizeof(int)*(maxlen));
-  int *av = (int *)malloc(sizeof(int)*(maxlen));
-  int *bv = (int *)malloc(sizeof(int)*(maxlen));
+
+  int maxlen = ls1_length > ls2_length ? ls1_length : ls2_length;
+  int maxlen_x2 = 2 * maxlen;
+
+
+  int *new_lengths = (int *)malloc(sizeof(int)*(maxlen_x2));
+  int *av = (int *)malloc(sizeof(int)*(maxlen_x2));
+  int *bv = (int *)malloc(sizeof(int)*(maxlen_x2));
+
 
   int i;
-  for (i=0; i < maxlen; i++) {
+  for (i=0; i < maxlen_x2; i++) {
     new_lengths[i] = -1;
+    av[i] = -1;
+    bv[i] = -1;
   }
 
   int nlx = 0;
@@ -59,13 +65,24 @@ static int * run_length(int *ls1, int ls1_length, int *ls2, int ls2_length) { //
 
   }
 
-  return new_lengths;
+  int j = maxlen_x2 - 1;
+  while (new_lengths[j] == -1) {
+    j--;
+  };
+
+  /* since length is end + 1 in 0-idx */
+  int final_size = j + 1;
+
+  struct Rle rle = {new_lengths, av, bv, final_size};
+
+  return rle;
+
 }
 
 
 static char * test_runlength(){
 
-  int size = 1e6;
+  int size = 1e3;
   int *ls1 = (int *)malloc(sizeof(int)*(size));
   int *ls2 = (int *)malloc(sizeof(int)*(size));
 
@@ -80,21 +97,174 @@ static char * test_runlength(){
 
   clock_t begin = clock();
 
-  /* here, do your time-consuming job */
-
-  int *new_lengths = run_length(ls1, size, ls2, size);
+  struct Rle rle = run_length(ls1, size, ls2, size);
 
   clock_t end = clock();
   double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 
   printf("Time used: %f\n", time_spent);
 
-  /* for(i = 0; i < 2 * size; i++) { */
-  /*     printf("%d ", new_lengths[i]); */
-  /*   } */
   printf("\n");
 
-  mu_assert("new_lengths not correct", new_lengths[0] == 1);
+  mu_assert("new_lengths not correct", rle.ls[0] == 1 && rle.ls[1] == 1);
+  mu_assert("rle.ls[rle.size - 1] is not correct", rle.ls[rle.size - 1] == 1);
+
+  return 0;
+
+}
+
+
+static char * test_runlength2(){
+
+  int SIZE=4;
+  static int SOURCE[4] = { 1, 2, 3, 4 };
+  int *ls1= malloc(sizeof(int)*SIZE);
+  memcpy(ls1, SOURCE, sizeof(int) * SIZE);
+
+
+  static int SOURCE2[4] = { 4, 3, 2, 1};
+  int *ls2= malloc(sizeof(int)*SIZE);
+  memcpy(ls2, SOURCE2, sizeof(int) * SIZE);
+
+
+  clock_t begin = clock();
+
+  struct Rle rle = run_length(ls1, 4, ls2, 4);
+
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+  printf("Time used: %f\n", time_spent);
+
+  printf("\n");
+
+  int i;
+  for (i=0; i<rle.size;i++) {
+    printf("l %i\n", rle.ls[i]);
+    printf("a %i\n", rle.a[i]);
+    printf("b %i\n\n", rle.b[i]);
+  };
+
+  mu_assert("rle.ls not correct", rle.ls[0] == 1 && rle.ls[1] == 2);
+  mu_assert("rle.ls[rle.size - 1] is not correct", rle.ls[rle.size - 1] == 1);
+
+
+  return 0;
+
+}
+
+
+static char * test_runlength3(){
+
+  int SIZE=1;
+  static int SOURCE[1] = { 1 };
+  int *ls1= malloc(sizeof(int)*SIZE);
+  memcpy(ls1, SOURCE, sizeof(int) * SIZE);
+
+
+  static int SOURCE2[1] = { 1 };
+  int *ls2= malloc(sizeof(int)*SIZE);
+  memcpy(ls2, SOURCE2, sizeof(int) * SIZE);
+
+
+  clock_t begin = clock();
+
+  struct Rle rle = run_length(ls1, 1, ls2, 1);
+
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+  printf("Time used: %f\n", time_spent);
+
+  printf("\n");
+
+  int i;
+  for (i=0; i<rle.size;i++) {
+    printf("l %i\n", rle.ls[i]);
+    printf("a %i\n", rle.a[i]);
+    printf("b %i\n", rle.b[i]);
+  };
+
+  mu_assert("rle.ls not correct", rle.ls[0] == 1);
+  mu_assert("rle.ls[rle.size - 1] is not correct", rle.ls[rle.size - 1] == 1);
+
+
+  return 0;
+
+}
+
+
+static char * test_runlength4(){
+
+  static int SOURCE[2] = { 1, 5};
+  int *ls1= malloc(sizeof(int)*2);
+  memcpy(ls1, SOURCE, sizeof(int) * 2);
+
+
+  static int SOURCE2[3] = { 1, 2, 3 };
+  int *ls2= malloc(sizeof(int)*3);
+  memcpy(ls2, SOURCE2, sizeof(int) * 3);
+
+
+  clock_t begin = clock();
+
+  struct Rle rle = run_length(ls1, 2, ls2, 3);
+
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+  printf("Time used: %f\n", time_spent);
+
+  printf("\n");
+
+  int i;
+  for (i=0; i<rle.size;i++) {
+    printf("l %i\n", rle.ls[i]);
+    printf("a %i\n", rle.a[i]);
+    printf("b %i\n", rle.b[i]);
+  };
+
+  mu_assert("rle.ls not correct", rle.ls[0] == 1);
+  mu_assert("rle.ls[rle.size - 1] is not correct", rle.ls[rle.size - 1] == 3);
+
+  return 0;
+
+}
+
+
+
+static char * test_runlength5(){
+
+  static int SOURCE[2] = { 1, 1};
+  int *ls1= malloc(sizeof(int)*2);
+  memcpy(ls1, SOURCE, sizeof(int) * 2);
+
+
+  static int SOURCE2[3] = { 1, 2, 3 };
+  int *ls2= malloc(sizeof(int)*3);
+  memcpy(ls2, SOURCE2, sizeof(int) * 3);
+
+
+  clock_t begin = clock();
+
+  struct Rle rle = run_length(ls1, 2, ls2, 3);
+
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+  printf("Time used: %f\n", time_spent);
+
+  printf("\n");
+
+  int i;
+  for (i=0; i<rle.size;i++) {
+    printf("l %i\n", rle.ls[i]);
+    printf("a %i\n", rle.a[i]);
+    printf("b %i\n", rle.b[i]);
+  };
+
+  mu_assert("rle.ls not correct", rle.ls[0] == 1);
+  mu_assert("rle.ls[rle.size - 1] is not correct", rle.ls[rle.size - 1] == 3);
 
   return 0;
 
@@ -103,8 +273,13 @@ static char * test_runlength(){
 
 static char * all_tests() {
   mu_run_test(test_runlength);
+  mu_run_test(test_runlength2);
+  mu_run_test(test_runlength3);
+  mu_run_test(test_runlength4);
+  mu_run_test(test_runlength5);
   return 0;
 }
+
 
 int main(int argc, char **argv) {
   char *result = all_tests();
